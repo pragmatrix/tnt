202bea8ca47ec2783be7d14a72ff21624cbdae17
Armin Sander <armin@replicator.org>
2018-09-04 11:13:04 +0200

    commandline string option does not seem to work

--- tnt/Program.fs
+++ tnt/Program.fs
@@ -13,37 +13,15 @@ type AddOptions = {
     Language: string
 
     [<Value(0, HelpText = "Relative path of the assembly file.")>]
-    Assembly: string option
+    Assembly: string
 }
 
 [<Verb("update", HelpText = "Extract all strings from all assemblies or one specific assembly and update the translation files.")>]
 type UpdateOptions = {
 
     [<Value(0, HelpText = "Relative path of the asseembly file.")>]
-    Assembly: string option
+    Assembly: string
 }
-
-(*
-
-type AddArgs =
-    | [<AltCommandLine("-l"); Mandatory>] Language of langauge: string
-    | [<AltCommandLine("-a")>] Assembly of assembly: string
-    with
-        interface IArgParserTemplate with
-            member this.Usage = 
-                match this with
-                | Language _ -> "The language code"
-                | Assembly _ -> "The relative path to the assembly that's used for extracting the original strings"
-type TNTArgs = 
-    | [<CliPrefix(CliPrefix.None)>] Add of ParseResults<AddArgs>
-    | [<CliPrefix(CliPrefix.None)>] Update
-    with
-        interface IArgParserTemplate with
-            member this.Usage =
-                match this with
-                | Add _ -> "Adds a new language"
-                | Update -> "Extracts all strings from the assemblies and updates the translations files."
-*)
 
 [<EntryPoint>]
 let main args =
@@ -64,10 +42,10 @@ let main args =
                 | :? AddOptions as opts -> 
                     API.add 
                         (LanguageIdentifier(opts.Language)) 
-                        (opts.Assembly |> Option.map AssemblyPath)
+                        (opts.Assembly |> Option.ofObj |> Option.map AssemblyPath)
                 | :? UpdateOptions as opts ->
                     API.update
-                        (opts.Assembly |> Option.map AssemblyPath)
+                        (opts.Assembly |> Option.ofObj |> Option.map AssemblyPath)
                 | x -> failwithf "internal error: %A" x
 
             let result = Output.run Console.WriteLine output
