25610944b1a8a8d5fbef62ddb882ccbb27bd590d
Armin Sander <armin@replicator.org>
2018-11-20 14:14:56 +0100

    protect worksheets, wrap text, and experiment with AdjustToContents

--- TNT.Library/Excel.fs
+++ TNT.Library/Excel.fs
@@ -42,23 +42,34 @@ module private Private =
 let export (file: File) : Excel = 
 
     use wb = new XLWorkbook();
-    wb.Protect()
 
     let ws = wb.Worksheets.Add(string file.ProjectName)
+    ws.RowHeight <- 0.
+
+    ignore ^ ws.Protect().SetFormatColumns().SetFormatRows()
+
     ws.Cell(1,1).Value <- sprintf "Original %s" (string file.SourceLanguage)
     ws.Cell(1,2).Value <- sprintf "Translated %s" (string file.TargetLanguage)
     ws.Cell(1,3).Value <- sprintf "State"
     ws.Cell(1,4).Value <- sprintf "Context"
     ws.Cell(1,5).Value <- sprintf "Notes"
-
     file.TranslationUnits
     |> Seq.iteri ^ fun i tu ->
         let row = i + 2
-        ws.Cell(row, 1).Value <- tu.Source
 
+        do
+            let sourceCell = ws.Cell(row, 1)
+            sourceCell.Value <- tu.Source
+
+            sourceCell.DataType <- XLDataType.Text
+            sourceCell.Style.Alignment.WrapText <- false
+        
         do
             let targetCell = ws.Cell(row, 2)
             targetCell.Value <- tu.Target
+            targetCell.DataType <- XLDataType.Text
+            targetCell.Style.Alignment.WrapText <- false
+
             ignore ^ targetCell.Style.Protection.SetLocked(false)
 
         do
@@ -70,6 +81,13 @@ let export (file: File) : Excel =
         ws.Cell(row, 4).Value <- tu.Contexts |> String.concat "\n"
         ws.Cell(row, 5).Value <- tu.Notes |> String.concat "\n"
 
+        // this enables proper auto-sizing
+        // https://github.com/ClosedXML/ClosedXML/issues/934
+        //ws.Row(row) (*.AdjustToContents(). *)ClearHeight()
+
+
+    ignore ^ ws.Columns().AdjustToContents(10., 200.)
+    
     use ms = new MemoryStream()
     wb.SaveAs(ms)
     Excel ^ ms.ToArray()
