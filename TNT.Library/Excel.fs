7c0a0241b0c210603efca41df7e940c999eeb500
Armin Sander <armin@replicator.org>
2018-11-21 20:44:34 +0100

    excel exporter: group sheets

--- TNT.Library/Excel.fs
+++ TNT.Library/Excel.fs
@@ -41,75 +41,82 @@ let export (file: File) : Excel =
 
     use wb = new XLWorkbook();
 
-    let ws = wb.Worksheets.Add(string file.ProjectName)
+    let setupWorksheet (name: string) (tus: TranslationUnit list) = 
 
-    ignore ^ ws.Protect().SetFormatColumns().SetFormatRows()
+        let ws = wb.Worksheets.Add(name)
+        ignore ^ ws.Protect().SetFormatColumns().SetFormatRows()
 
-    ws.Cell(1,1).Value <- sprintf "%s" (file.SourceLanguage.Formatted)
-    ws.Cell(1,2).Value <- sprintf "%s" (file.TargetLanguage.Formatted)
-    ws.Cell(1,3).Value <- sprintf "State"
-    ws.Cell(1,4).Value <- sprintf "Contexts"
-    ws.Cell(1,5).Value <- sprintf "Notes"
+        ws.Cell(1,1).Value <- sprintf "%s" (file.SourceLanguage.Formatted)
+        ws.Cell(1,2).Value <- sprintf "%s" (file.TargetLanguage.Formatted)
+        ws.Cell(1,3).Value <- sprintf "State"
+        ws.Cell(1,4).Value <- sprintf "Contexts"
+        ws.Cell(1,5).Value <- sprintf "Notes"
     
-    ws.Row(1).Style.Font.Bold <- true
+        ws.Row(1).Style.Font.Bold <- true
+    
+        file.TranslationUnits
+        |> Seq.iteri ^ fun i tu ->
+            let row = i + 2
+
+            // source
+            do
+                let cell = ws.Cell(row, 1)
+                cell.Value <- tu.Source
+                cell.DataType <- XLDataType.Text
+
+                cell.Style.Alignment.WrapText <- true
+                cell.Style.Alignment.Vertical <- XLAlignmentVerticalValues.Center
+
+            // target        
+            do
+                let cell = ws.Cell(row, 2)
+                cell.Value <- tu.Target
+                cell.DataType <- XLDataType.Text
+
+                cell.Style.Alignment.WrapText <- true
+                cell.Style.Alignment.Vertical <- XLAlignmentVerticalValues.Center
+
+                ignore ^ cell.Style.Protection.SetLocked(false)
+
+            // state
+            do
+                let cell = ws.Cell(row, 3)
+                cell.Value <- TargetState.toString tu.State
+                cell.DataValidation.List(StateValidationList, true)
+
+                cell.Style.Alignment.Vertical <- XLAlignmentVerticalValues.Center
+
+                ignore ^ cell.Style.Protection.SetLocked(false)
+
+            // contexts
+            do         
+                let cell = ws.Cell(row, 4)
+                cell.Value <- tu.Contexts |> String.concat "\n"
+                cell.DataType <- XLDataType.Text
+
+                cell.Style.Alignment.WrapText <- true
+                cell.Style.Alignment.Vertical <- XLAlignmentVerticalValues.Center
+
+            // notes
+            do
+
+                let cell = ws.Cell(row, 5)
+                // two newlines for note separation (because notes may consist of multiple lines).
+                cell.Value <- tu.Notes |> String.concat "\n\n"
+                cell.DataType <- XLDataType.Text
+
+                cell.Style.Alignment.WrapText <- true
+                cell.Style.Alignment.Vertical <- XLAlignmentVerticalValues.Center
+
+        // autosize columns.
+        ignore ^ ws.Columns().AdjustToContents(10., 100.)
     
     file.TranslationUnits
-    |> Seq.iteri ^ fun i tu ->
-        let row = i + 2
+    |> List.groupBy ^ fun tu -> TargetState.toString tu.State
+    |> Seq.filter ^ fun (_, units) -> units <> []
+    |> Seq.iter ^ fun (stateName, units) ->
+        setupWorksheet (string file.ProjectName + " - " + stateName) units
 
-        // source
-        do
-            let cell = ws.Cell(row, 1)
-            cell.Value <- tu.Source
-            cell.DataType <- XLDataType.Text
-
-            cell.Style.Alignment.WrapText <- true
-            cell.Style.Alignment.Vertical <- XLAlignmentVerticalValues.Center
-
-        // target        
-        do
-            let cell = ws.Cell(row, 2)
-            cell.Value <- tu.Target
-            cell.DataType <- XLDataType.Text
-
-            cell.Style.Alignment.WrapText <- true
-            cell.Style.Alignment.Vertical <- XLAlignmentVerticalValues.Center
-
-            ignore ^ cell.Style.Protection.SetLocked(false)
-
-        // state
-        do
-            let cell = ws.Cell(row, 3)
-            cell.Value <- TargetState.toString tu.State
-            cell.DataValidation.List(StateValidationList, true)
-
-            cell.Style.Alignment.Vertical <- XLAlignmentVerticalValues.Center
-
-            ignore ^ cell.Style.Protection.SetLocked(false)
-
-        // contexts
-        do         
-            let cell = ws.Cell(row, 4)
-            cell.Value <- tu.Contexts |> String.concat "\n"
-            cell.DataType <- XLDataType.Text
-
-            cell.Style.Alignment.WrapText <- true
-            cell.Style.Alignment.Vertical <- XLAlignmentVerticalValues.Center
-
-        // notes
-        do
-
-            let cell = ws.Cell(row, 5)
-            // two newlines for note separation (because notes may consist of multiple lines).
-            cell.Value <- tu.Notes |> String.concat "\n\n"
-            cell.DataType <- XLDataType.Text
-
-            cell.Style.Alignment.WrapText <- true
-            cell.Style.Alignment.Vertical <- XLAlignmentVerticalValues.Center
-
-    // autosize columns.
-    ignore ^ ws.Columns().AdjustToContents(10., 100.)
-    
     use ms = new MemoryStream()
     wb.SaveAs(ms)
     Excel ^ ms.ToArray()
