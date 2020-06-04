7ab55d12327bdeff5d41a7b9bba85d92e46ab996
Armin Sander <armin@replicator.org>
2020-06-04 11:31:49 +0200

    clarify comment

--- tnt/Program.fs
+++ tnt/Program.fs
@@ -1,10 +1,11 @@
 ﻿open System
 open TNT.Model
+open TNT.Library
 open TNT.Library.Output
 open TNT.Library.ExportModel
 open FunToolbox.FileSystem
 open CommandLine
-// for some reason, this must come last
+// for some reason, this must be opened here a second time, otherwise compilation will fail
 open TNT.Library
 
 [<Verb("init", HelpText = "Initialize the current directory. Creates the '.tnt' directory and the 'sources.json' file.")>]
